881a744166b4fbeb291ad198e0568ae36e5f1953
Marat2004 <testinger2021@yandex.ru>
2022-07-09 18:18:03 +0500

    -

---
+++ мини-игра (гладиатор)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace мини_игра__гладиатор_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
