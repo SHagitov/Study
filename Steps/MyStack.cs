69a9b2179fa4f01ead0770e321500d8f828d0bc0
Marat2004 <testinger2021@yandex.ru>
2022-07-29 11:23:25 +0500

    -

---
+++ Steps/MyStack.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Steps
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private T[] _items;
+
+        public int Count { get; private set; }
+
+        public int Capacity
+        {
+            get { return _items.Length; }
+        }
+        public MyStack()
+        {
+            const int defaultCapacity = 4;
+            _items = new T[defaultCapacity];
+        }
+
+        public MyStack(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            if (_items.Length == Count)
+            {
+                T[] largerArray = new T[Count * 2];
+                Array.Copy(_items, largerArray, Count);
+                _items = largerArray;
+            }
+            _items[Count++] = item;
+        }
+
+        public void Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            _items[--Count] = default(T);
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _items[Count - 1];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
