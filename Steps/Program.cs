19d75b99e09f124e979d29ef7d1208e0a5922104
Marat2004 <testinger2021@yandex.ru>
2022-07-31 12:47:47 +0500

    -

--- Steps/Program.cs
+++ Steps/Program.cs
@@ -7,12 +7,85 @@ using System.Threading;
 
 namespace Steps
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            MathLogicalOperations();
+            Random();
             Console.ReadLine();
+        }
+        static void Random()
+        {
+            Random randomize = new Random();    // Создание переменной randomize для получения псевослучайных чисел
+
+            Console.WriteLine($"int.MaxValue = {int.MaxValue}");
+            Console.WriteLine($"int.MinValue = {int.MinValue}\n");
+
+            #region Замечание
+
+            //Console.WriteLine($"byte.MaxValue = {byte.MaxValue}");
+            //Console.WriteLine($"byte.MinValue = {byte.MinValue}\n");
+
+            //Console.WriteLine($"uint.MaxValue = {uint.MaxValue}");
+            //Console.WriteLine($"uint.MinValue = {uint.MinValue}\n");
+
+            //Console.WriteLine($"long.MaxValue = {long.MaxValue}");
+            //Console.WriteLine($"long.MinValue = {long.MinValue}\n");
+
+            #endregion
+
+            int randomIntResult = randomize.Next();     // Возвращает неотрицательное случайное целое число
+                                                        // 32-разрядное целое число со знаком, 
+                                                        // которое больше или равно 0 и меньше чем Int32.MaxValue.
+            Console.WriteLine($"Пример 1. randomIntResult = {randomIntResult}");
+
+            randomIntResult = randomize.Next(100);      // Возвращает неотрицательное случайное целое число, 
+                                                        // которое меньше максимально допустимого значения
+                                                        // от 0 до 100, не включая 100 
+            Console.WriteLine($"Пример 2. randomIntResult = {randomIntResult}");
+
+            randomIntResult = randomize.Next(10, 100);  // Возвращает неотрицательное случайное целое число, 
+                                                        // которое больше минимально допустимого значения
+                                                        // и меньше максимально допустимого значения
+                                                        // от 10 до 100, не включая 100 
+
+            Console.WriteLine($"Пример 3. randomIntResult = {randomIntResult}");
+
+            double randomDoubleResult = randomize.NextDouble() + randomize.Next(100); // Возвращает случайное число с плавающей запятой, 
+                                                                                      // которое больше или равно 0.0 и меньше 1.0
+                                                                                      // не включая 1.0
+            Console.WriteLine($"Пример 4. randomDoubleResult = {randomDoubleResult}");
+
+
+        }
+        static void DataRead()
+        {
+            // Вывод данных
+            string name = "Билл Гейтс";
+            byte age = 63;
+            ushort costs = 118;
+
+            #region _
+            //Ввод данных
+
+            Console.WriteLine("Введите имя: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст: ");
+            age = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сумму ежемесячных трат: ");
+            costs = Convert.ToUInt16(Console.ReadLine());
+
+            #endregion
+
+            Console.WriteLine("Имя: " + name + " Возраст: " + age + " Расходы: $" + costs); // конкатенация строк
+            Console.WriteLine("Имя: {0} Возраст: {1} Расходы: ${2}", name, age, costs);     // форматированный вывод
+            Console.WriteLine($"Имя: {name} Возраст: {age} Расходы: ${costs}");             // интерполяция строк
+
+            Console.ReadKey();
+
+
         }
         static void MathLogicalOperations()
         {
