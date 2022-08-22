3ea1c0888230dc64a1cbd467b63f0eaa4d40b67d
Marat2004 <testinger2021@yandex.ru>
2022-08-22 14:09:07 +0500

    -

--- Steps/Program.cs
+++ Steps/Program.cs
@@ -13,7 +13,7 @@ namespace Steps
     {
         private static void Main(string[] args)
         {
-            Example0();
+            MyyClass();
             Console.ReadLine();
         }
 
@@ -186,6 +186,29 @@ namespace Steps
 
         }
 
+        class A { }
+        class B : A { }
+        class C : A { }
+        class D : A { }
+        class E : B { }
+        class MyClass<T1, T2, T3>
+            where T1 : T2
+            where T3 : T1
+        {
+
+        }
+        static void MyyClass()
+        {
+            MyClass<B, A, E> my = new MyClass<B, A, E>();
+            //MyClass<C, A, A> myClass1 = new MyClass<C, A, A>();
+            //MyClass<E, A, A> myClass2 = new MyClass<E, A, A>();
+            //MyClass<E, B, C> myClass3 = new MyClass<E, B, C>();
+
+            //MyClass<string, object, int> myClass4 = new MyClass<string, object, int>();
+            //MyClass<int, object, int> myClass5 = new MyClass<int, object, int>();
+            //MyClass<int, ValueType, int> myClass6 = new MyClass<int, ValueType, int>();
+            //MyClass<ValueType, object, int> myClass7 = new MyClass<ValueType, object, int>();
+        }
         private class Class1<T>
         where T : new()
         {
