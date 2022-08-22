e3995024a0f520ba9c211ed3e8d4c98c41e983d1
Marat2004 <testinger2021@yandex.ru>
2022-08-22 17:45:06 +0500

    -

--- Steps/Program.cs
+++ Steps/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Xml.Linq;
@@ -11,32 +12,109 @@ namespace Steps
 
     public static class Program
     {
-        #region ToDo
-
-        // void, void
-        public delegate void DelegateName1();
-
-        // void, string
-        public delegate void DelegateName2(string Message);
-
-        // double, double
-        public delegate double DelegateName3(double Args);
-
-        #endregion
-        // void, void
-        private static void Something1() { Console.WriteLine("Something method"); }
-
-        // void, string
-        private static void Something2(string Msg) { Console.WriteLine(Msg); }
-
-        // double, double
-        private static double Something3(double X) { return X + 10; }
         private static void Main(string[] args)
         {
             Definitions();
             Console.ReadLine();
         }
 
+        class  Delegates
+        {
+            public static void Void1() { }
+
+            //delegate void VoidDelegate2(int Argument1);
+            //delegate void VoidDelegate3(double Argument1);
+            //delegate void VoidDelegate4(string Argument1);
+
+
+            delegate void VoidDelegate<T>(T Args);
+
+            public static void Void2(int Args1) { }
+            public static void Void3(double Args1) { }
+            public static void Void4(string Args1) { }
+
+
+            delegate void VoidDelegate5(int Argument1, double Argument2);
+            public static void Void5(int Args1, double Args2) { }
+
+
+            //delegate void VoidDelegate6(int Argument1, double Argument2, string Argument3);
+            //delegate void VoidDelegate7(string Argument1, double Argument2, int Argument3);
+            //delegate void VoidDelegate8(int Argument1, int Argument2, int Argument3);
+
+            delegate void VoidDelegate<T1, T2, T3>(T1 Argument1, T2 Argument2, T3 Argument3);
+            public static void Void6(int Args1, double Args2, string Args3) { }
+            public static void Void7(string Args1, double Args2, int Args3) { }
+            public static void Void8(int Args1, int Args2, int Args3) { }
+
+            // ===============================================================================
+
+            delegate int ReturnDelegate1();
+            public static int Return1() { return 0; }
+
+            //delegate int ReturnDelegate2(int Argument1);
+            //delegate string ReturnDelegate3(double Argument1);
+            //delegate double ReturnDelegate4(string Argument1);
+
+            delegate TOut ReturnDelegate<TIn, TOut>(TIn Argument1);
+
+            public static int Return2(int Args1) { return 0; }
+            public static string Return3(double Args1) { return String.Empty; }
+            public static double Return4(string Args1) { return 0; }
+
+            delegate bool ReturnDelegate5(int Argument1, double Argument2);
+            public static bool Return5(int Args1, double Args2) { return true; }
+
+            //delegate char ReturnDelegate6(int Argument1, double Argument2, string Argument3);
+            //delegate long ReturnDelegate7(string Argument1, double Argument2, int Argument3);
+            //delegate byte ReturnDelegate8(int Argument1, int Argument2, int Argument3);
+
+            delegate TOut ReturnDelegate<TIn1, TIn2, TIn3, TOut>(TIn1 Argument1, TIn2 Argument2, TIn3 Argument3);
+
+            public static char Return6(int Args1, double Args2, string Args3) { return char.MaxValue; }
+            public static long Return7(string Args1, double Args2, int Args3) { return 0; }
+            public static byte Return8(int Args1, int Args2, int Args3) { return 0; }
+
+            // ===============================================================================
+
+
+            //delegate bool ReturnBool1(int Argument1);
+            //delegate bool ReturnBool2(string Argument1);
+            //delegate bool ReturnBool3(double Argument1);
+            //delegate bool ReturnBool4(int Argument1);
+
+
+            delegate bool PredicateDelegate<TIn>(TIn Argument1);
+
+            public static bool BoolMethod1(int Args1) { return true; }
+            public static bool BoolMethod2(string Args1) { return true; }
+            public static bool BoolMethod3(double Args1) { return true; }
+            public static bool BoolMethod4(byte Args1) { return true; }
+
+            VoidDelegate<int> var1 = Void2;
+            VoidDelegate<double> var2 = Void3;
+            VoidDelegate<string> var3 = Void4;
+
+            VoidDelegate<int, double, string> var6 = Void6;
+            VoidDelegate<string, double, int> var7 = Void7;
+            VoidDelegate<int, int, int> var8 = Void8;
+
+            ReturnDelegate<int, int> var9 = Return2;
+            ReturnDelegate<double, string> var10 = Return3;
+            ReturnDelegate<string, double> var11 = Return4;
+
+
+            ReturnDelegate<int, double, string, char> var12 = Return6;
+            ReturnDelegate<string, double, int, long> var13 = Return7;
+            ReturnDelegate<int, int, int, byte> var14 = Return8;
+
+            PredicateDelegate<int> var15 = BoolMethod1;
+
+            PredicateDelegate<string> var16 = BoolMethod2;
+            PredicateDelegate<double> var17 = BoolMethod3;
+            PredicateDelegate<byte> var18 = BoolMethod4;
+
+        }
         private static void Definitions()
         {
             // var r1 = Something1;
@@ -459,6 +537,27 @@ namespace Steps
 
         }
 
+        #region ToDo
+
+        // void, void
+        public delegate void DelegateName1();
+
+        // void, string
+        public delegate void DelegateName2(string Message);
+
+        // double, double
+        public delegate double DelegateName3(double Args);
+
+        #endregion
+
+        // void, void
+        private static void Something1() { Console.WriteLine("Something method"); }
+
+        // void, string
+        private static void Something2(string Msg) { Console.WriteLine(Msg); }
+
+        // double, double
+        private static double Something3(double X) { return X + 10; }
         private class A { }
 
         private class B : A { }
