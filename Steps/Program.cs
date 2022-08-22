6f43e7e48d4db235676121ea67d5ca4fb197606b
Marat2004 <testinger2021@yandex.ru>
2022-08-22 15:33:13 +0500

    -

--- Steps/Program.cs
+++ Steps/Program.cs
@@ -44,6 +44,60 @@ namespace Steps
             Console.Clear();
 
             #endregion
+
+            //namespace Example
+            //{
+
+            //    abstract class Animal
+            //    {
+            //        public string Name { get; private set; }
+            //        public Animal(string Name) { this.Name = Name; }
+            //    }
+
+            //    class Bird : Animal
+            //    {
+            //        public Bird(string Name) : base(Name) { }
+            //    }
+
+            //    #region ToDo Важно
+            //    // interface IAnimal<out T>
+
+            //    // T GetAnimal { get; set; }
+            //    // void Method(T args);
+            //    // Ошибка CS1961  Недопустимое отклонение: 
+            //    // Параметр типа "T" должен быть инвариантно, допустимым на "IAnimal<T>.GetAnimal". 
+            //    // "T" является ковариантный.Example_1333    
+
+            //    #endregion
+
+            //    interface IAnimal<out T>
+            //        where T : Animal
+            //    {
+            //        T GetValue { get; }
+            //        T GetValueMethod(); 
+            //    }
+
+            //    class Kiwi : IAnimal<Bird>
+            //    {
+            //        public Bird GetValue { get { return new Bird("KiwiBirdP"); } }
+
+            //        public Bird GetValueMethod() { return new Bird("KiwiBirdM"); }
+            //    }
+
+
+            //    class Program
+            //    {
+            //        static void Main(string[] args)
+            //        {
+            //            Kiwi concreteKiwi = new Kiwi();
+            //            IAnimal<Bird> concreteBirdIAnimal = concreteKiwi;
+            //            IAnimal<Animal> concreteAnimalIAnimal = concreteKiwi; // Нельзя т к типы инвариантны
+            //            // Ковариантность даёт возможность использовать более конкретный тип, чем заданный изначально
+            //        }
+            //    }
+            //}
+
+
         }
         private static void Invariance()
         {
