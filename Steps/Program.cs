e6e4d7a615d740bf8502f4cbf666677e90c502a4
Marat2004 <testinger2021@yandex.ru>
2022-08-22 14:00:01 +0500

    -

--- Steps/Program.cs
+++ Steps/Program.cs
@@ -13,10 +13,68 @@ namespace Steps
     {
         private static void Main(string[] args)
         {
-            Temporary();
+            Example0();
             Console.ReadLine();
         }
 
+        static void Example0()
+        {
+            //static void Swap(ref int A, ref int B) { int c = A; A = B; B = c; }
+            //static void Swap(ref double[] A, ref double[] B) { double[] c = A; A = B; B = c; }
+            //static void Swap(ref byte[] A, ref byte[] B) { byte[] c = A; A = B; B = c; }
+
+            #region T
+
+            static void Swap<T>(ref T A, ref T B) { T c = A; A = B; B = c; }
+
+            #endregion
+
+            #region 
+
+            int a = 1, b = 2;
+            Console.WriteLine($"a = {a}  b = {b} ");
+            Swap<int>(ref a, ref b);
+            Console.WriteLine($"a = {a}  b = {b} "); Console.WriteLine();
+
+            double[] x = { 1.1, 2.3, 4.5 };
+            double[] y = { 6.7, 8.9, 10 };
+
+            Console.Write("x: "); foreach (var e in x) Console.Write($"{e} "); Console.WriteLine();
+            Console.Write("y: "); foreach (var e in y) Console.Write($"{e} "); Console.WriteLine();
+
+            Swap<double[]>(ref x, ref y);
+
+            Console.Write("x: "); foreach (var e in x) Console.Write($"{e} "); Console.WriteLine();
+            Console.Write("y: "); foreach (var e in y) Console.Write($"{e} "); Console.WriteLine(); Console.WriteLine();
+
+            byte[] k = { 1, 2, 4 };
+            byte[] l = { 6, 8, 10 };
+
+            Console.Write("k: "); foreach (var e in k) Console.Write($"{e} "); Console.WriteLine();
+            Console.Write("l: "); foreach (var e in l) Console.Write($"{e} "); Console.WriteLine();
+
+            Swap<byte[]>(ref k, ref l);
+
+            Console.Write("k: "); foreach (var e in k) Console.Write($"{e} "); Console.WriteLine();
+            Console.Write("l: "); foreach (var e in l) Console.Write($"{e} "); Console.WriteLine();
+
+            #endregion
+
+            #region T
+
+            Console.WriteLine("\n---\n");
+
+            Swap<int>(ref a, ref b);
+            Console.WriteLine($"a = {a}  b = {b} "); Console.WriteLine();
+
+            Swap<byte[]>(ref k, ref l);
+            Console.Write("k: "); foreach (var e in k) Console.Write($"{e} "); Console.WriteLine();
+            Console.Write("l: "); foreach (var e in l) Console.Write($"{e} "); Console.WriteLine();
+
+
+
+            #endregion
+        }
         private static void Temporary()
         {
             #region 
