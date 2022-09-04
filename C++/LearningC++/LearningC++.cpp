29eca7ad4eaefdc6631e1355c20f1238c08d5173
Marat2004 <testinger2021@yandex.ru>
2022-09-04 11:07:25 +0500

    -

---
+++ C++/LearningC++/LearningC++.cpp
@@ -0,0 +1,29 @@
+﻿// LearningC++.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 65;
+    a = 10;
+    float t = 12.34f;
+    double z = 34.78750;
+
+    bool var = true;
+
+    char n;
+    n = 'n';
+    n = '0';
+    const float PI = 3.1415f;
+
+    int _a = 10;
+    int A_123 = 0;
+
+    std::cout << "Hello, World!\n";
+    cout << "Hello, World!" << endl;
+    //char v;
+    //cin >> v;
+    //cout << v;
+
+}
