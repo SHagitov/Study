88bc4da5b67ec9f3ce0b9fb6758dc380b2ce8cb4
Marat2004 <testinger2021@yandex.ru>
2023-03-26 15:19:35 +0500

    -

--- InfEge/17r.py
+++
@@ -1,7 +0,0 @@
-a = [int(x) for x in open('17.txt')]
-k5 = len([i for i in a if i % 5 == 0])
-ans = []
-for i1, i2 in zip(a, a[1:]):
-    if (((i1 < 0) + (i2 < 0)) == 1) and (i1 + i2 < k5):
-        ans += [i1 + i2]
-print(len(ans), max(ans))
